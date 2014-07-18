f1c678b668168ad72c3e0a9c647c033bfe657f4f
Hristo Ivanov <hristo.idgh@gmail.com>
2014-07-18 09:59:27 +0200

    Tests changed to return the assertion fail rather than a generic error message

--- tests/CountsPettioner_tests.py
+++ tests/CountsPettioner_tests.py
@@ -9,27 +9,27 @@ from CountsPettioner import CountsPettioner as CP
 class CountsPettionerTestCase(unittest.TestCase):
 	def test_Equality_aux(self):
 		response=CP.aux(['hola','hello'])
-		self.assertEqual(response, '["hola", "hello"]', 'Incorrect json dump')
+		self.assertEqual(response, '["hola", "hello"]')
 
 		response=CP.aux({'color':'red','fruits':['apple','pear','strawberry','banana'],'numbers':[1,2,3,4,5]})
-		self.assertEqual(response, '{"color": "red", "numbers": [1, 2, 3, 4, 5], "fruits": ["apple", "pear", "strawberry", "banana"]}', 'Incorrect json dump')
+		self.assertEqual(response, '{"color": "red", "numbers": [1, 2, 3, 4, 5], "fruits": ["apple", "pear", "strawberry", "banana"]}')
 
 	def test_Equality_reset_to_0(self):
 		aux=[234,432,[43243,4324,4234,[324,324,[3445]]]]
 		CP.reset_to_0(aux)
-		self.assertEqual(aux, [0, 0, [0, 0, 0, [0, 0, [0]]]], 'Incorrest reset to 0')
+		self.assertEqual(aux, [0, 0, [0, 0, 0, [0, 0, [0]]]])
 
 	def test_Eqiality_copy_and_reset(self):
 		aux1=[[345,321],[3452,421],[43243,4324,4234,[324,324,[3445]]]]
 		aux2=[[345,321],[3452,421],[43243,4324,4234,[324,324,[3445]]]]
 		response=CP.copy_and_reset(aux1,False)
-		self.assertEqual(aux2, response, 'Incorrect copy in copy and reset')
+		self.assertEqual(aux2, response)
 		self.assertEqual(aux1,[[0,0],[3452,421],[43243,4324,4234,[324,324,[3445]]]])
 
 		aux1=[[345,321],[3452,421],[43243,4324,4234,[324,324,[3445]]]]
 		aux2=[[345,321],[3452,421],[43243,4324,4234,[324,324,[3445]]]]
 		response=CP.copy_and_reset(aux1,True)
-		self.assertEqual(aux2, response, 'Incorrect copy in copy and reset')
+		self.assertEqual(aux2, response)
 		self.assertEqual(aux1,[[0,0],[0,0],[0,0,0,[0,0,[0]]]])
 
 
@@ -38,20 +38,19 @@ class CountsPettionerTestCase(unittest.TestCase):
 		database_adapter=None
 		CP_instance=CP(None, None, None, None, None, database_adapter)
 		with capture(CP_instance.save_BinTable,1405516380.0, [23,12,45,23,12,67]) as output:
-			self.assertEqual(output,'start_date_time: 2014-07-16 15:13:00 Counts: [23, 12, 45, 23, 12, 67]\n','Nah man')		
+			self.assertEqual(output,'start_date_time: 2014-07-16 15:13:00 Counts: [23, 12, 45, 23, 12, 67]\n')		
 
 
 	def test_DatabaseCalls_save_BinTable(self):
 		database=MagicMock()
-		database.execute=MagicMock(return_value=True)
-		database.commit=MagicMock(return_value=True)
+		database.execute.return_value=True
+		database.commit.return_value=True
 
 		#Instance of a CountsPettioner where everythong is None except the database_adapter.
 		CP_instance=CP(None, None, None, None, None, database)
 		CP_instance.save_BinTable(1405516380.0,[4 for x in xrange(18)])
 
-		database.execute.assert_called_with(mock.ANY)
-		database.commit.assert_called_with()
+		database.assert_has_calls([call.execute(mock.ANY), call.commit()], any_order=False)
 
 	def test_OutPut_save_EventsInfo(self):
 		#Instance of a CountsPettioner where everythong is None.
@@ -63,19 +62,18 @@ class CountsPettionerTestCase(unittest.TestCase):
 
 	def test_DatabaseCalls_save_EventsInfo(self):
 		database=MagicMock()
-		database.execute=MagicMock(return_value=True)
-		database.commit=MagicMock(return_value=True)
+		database.execute.return_value=True
+		database.commit.return_value=True
 	
 		#Instance of a CountsPettioner where everythong is None except the database_adapter.
 		CP_instance=CP(None, None, None, None, None, database)
 		CP_instance.save_EventsInfo(1405516380.0,[None,[[4 for x in xrange(128)] for x in xrange(18)],[4 for x in xrange(18)],[0 for x in xrange(18)]])
 
-		database.execute.assert_called_with(mock.ANY)
-		database.commit.assert_called_with()
+		database.assert_has_calls([call.execute(mock.ANY), call.commit()], any_order=False)
 
 	def test_Equality_get_min(self):
 		response=CP.get_min(1405524513.631769)
-		self.assertEqual(response,1405524480.0,'Minute calculated uncorrectly')
+		self.assertEqual(response,1405524480.0)
 
 		response=CP.get_min(123.3)
 		self.assertEqual(response,120.0)
--- tests/bm35_tests.py
+++ tests/bm35_tests.py
@@ -7,20 +7,20 @@ from bm35 import bm35
 class bm35TestCase(unittest.TestCase):
 	def test_Equality_compute_crc(self):
 		response=bm35.bm35_compute_crc('AAA')
-		self.assertEqual(response,'AAA195','Incorrect crc computed')
+		self.assertEqual(response,'AAA195')
 		
 		response=bm35.bm35_compute_crc('HiThere')
-		self.assertEqual(response,'HiThere681','Incorrect crc computed')
+		self.assertEqual(response,'HiThere681')
 
 		response=bm35.bm35_compute_crc('aaa')
-		self.assertNotEqual(response,'aaa195','Incorrest crc computed')
+		self.assertNotEqual(response,'aaa195')
 
 	def test_Equality_parse_pressure_answer(self):
 		response=bm35.bm35_parse_pressure_answer('M99D08.02.1999,11:40,96502,96555,0785')
-		self.assertEqual(response,{'date':'1999-02-08 11:40:00','meanPressure':96502,'instantPressure':96555}, 'Answer parsed Incorrectly')
+		self.assertEqual(response,{'date':'1999-02-08 11:40:00','meanPressure':96502,'instantPressure':96555})
 
 		response=bm35.bm35_parse_pressure_answer('M99D17.07.2014,12:47,85661,11555,0768')
-		self.assertEqual(response,{'date':'2014-07-17 12:47:00','meanPressure':85661,'instantPressure':11555}, 'Answer parsed Incorrectly')
+		self.assertEqual(response,{'date':'2014-07-17 12:47:00','meanPressure':85661,'instantPressure':11555})
 
 	def test_Errors_parse_pressure_answer(self):
 		self.assertRaisesRegexp(ValueError,'Invalid answer to parse', bm35.bm35_parse_pressure_answer, 'A incorrect string, just to test')
