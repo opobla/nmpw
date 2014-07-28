9a4b4a459d5c16efb12ccb9ec642a947184fb114
hristoivanov <hristo.idgh@gmail.com>
2014-07-28 10:23:57 +0000

    Changes made to read blocks of bytes rather than reading byte by byte. This allows us to handle more events.

--- Reader.py
+++ Reader.py
@@ -24,119 +24,132 @@ class Reader(threading.Thread):
 		self.shared_counts_data[:]=[0 for x in xrange(18)]
 		self.shared_events_data[:]=[[0 for x in xrange(18)],[[0 for x in xrange(128)] for x in xrange(18)],[0 for x in xrange(18)],[0 for x in xrange(18)]]
 
+		self.channel_counts=None
+
+		self.overflow=None
+
+		self.channel=None
+		self.count06=None
+		self.count711=None
+		self.pulse_type=None
+
+		self.counts_val=None
+		self.counts_values=None
+
+	def process(self, next):
+		#------------------Overflows
+		if ((ord(next[0]) & 0b11000000)== 0b00000000):
+			self.status='Ovbyte1'
+			if self.channel_counts!=None:
+				self.channel_counts=None
+				logging.info(self.name+': Error:Error while reading the counts.')
+			self.overflow=[0 for x in xrange(18)]
+			self.overflow[0:6]=[int(x) for x in '{0:06b}'.format(ord(next[0]) & 0b00111111)[::-1]]
+			return
+
+		if self.status=='Ovbyte1' and ((ord(next[0]) & 0b10000000) == 0b10000000):
+			self.status='Ovbyte2'
+			self.overflow[6:13]=[int(x) for x in '{0:07b}'.format(ord(next[0]) & 0b01111111)[::-1]]
+			return
+
+		if self.status=='Ovbyte2' and ((ord(next[0]) & 0b10000000) == 0b10000000):
+			self.status='bytex'
+			self.overflow[13:]=[int(x) for x in '{0:05b}'.format(ord(next[0]) & 0b00011111)[::-1]]
+			overflow_general=(ord(next[0]) & 0b00100000) >> 5
+			overflow_almost_full=(ord(next[0]) & 0b01000000) >> 6
+
+			print 'Overflow: ',self.overflow
+			self.shared_events_data[3][:]=map(operator.add, self.shared_events_data[3],self.overflow)
+			#  TODO Decide what to do with the overflow_general and almost_full data...
+			return
+	
+		#----------------Pulse Widths
+		if ((ord(next[0]) & 0b11100000) == 0b01000000):
+			self.status="byte1"
+			if self.channel_counts!=None:
+				self.channel_counts=None
+				logging.info(self.name+': Error:Error while reading the counts.')
+			self.channel=ord(next[0]) & 0b00011111
+			return
+
+		if  self.status=="byte1" and ((ord(next[0]) & 0b10000000) == 0b10000000):
+			self.status="byte2"
+			self.count06=ord(next[0]) & 0b01111111
+			return
+
+		if self.status=="byte2" and ((ord(next[0]) & 0b10000000) == 0b10000000):
+			self.status="bytex"
+			self.count711=ord(next[0]) & 0b00011111
+			self.pulse_type=(ord(next[0]) & 0b00100000) >> 5
+			pulse_width_ticks=self.count06 + (self.count711 << 7)
+
+			if self.pulse_type==0:
+				self.shared_events_data[2][self.channel] +=1
+			if self.pulse_type==1:
+				self.shared_events_data[1][self.channel][pulse_width_ticks>>5] +=1
+				self.shared_events_data[0][self.channel] +=1
+			
+
+			#print datetime.datetime.now().time(),"Ch:",self.channel,"Pulse type:",self.pulse_type," Pulse width:",pulse_width_ticks,\
+			#	float(pulse_width_ticks) / 50.0,"useg"
+			return
+
+
+		#----------------Counts
+		if ((ord(next[0]) & 0b11100000) == 0b01100000):
+			self.status='Contbyte3'
+			print 'Counts transmision started...'
+			if self.channel_counts!=None:
+				logging.info(self.name+': Error:Error while reading the counts.')
+			self.channel_counts=0
+			self.counts_val=0
+			self.counts_values=[0 for x in xrange(18)]
+			return
+
+		if self.status=='Contbyte3' and ((ord(next[0]) & 0b10000000) == 0b10000000):
+			self.status='Contbyte1'
+			self.counts_val=ord(next[0]) & 0b01111111
+			return
+
+		if self.status=='Contbyte1' and ((ord(next[0]) & 0b10000000) == 0b10000000):
+			self.status='Contbyte2'
+			self.counts_val=self.counts_val+((ord(next[0]) & 0b01111111) << 7)
+			return
+
+		if self.status=='Contbyte2' and ((ord(next[0]) & 0b10000000) == 0b10000000):
+			self.status='Contbyte3'
+			self.counts_val=self.counts_val+((ord(next[0]) & 0b00000011) << 14)
+			self.counts_values[self.channel_counts]=self.counts_val
+			if self.channel_counts==17:
+				self.shared_counts_data[:]=self.counts_values[:]
+				self.counts_condition.release()
+
+				self.channel_counts=None
+				self.status='bytex'
+			else:
+				self.channel_counts +=1
+			return
+
+
+
+		#--------Error, we have received something that wasnt expected
+		if self.channel_counts!=None:
+			logging.info(self.name+': Error:Error while reading the counts.')
+		self.status='bytex'
+		#  TODO maybe restart all the varibles {self.channel_counts}
+		#  TODO keep acounts of unexpected reads
+		logging.info(self.name+':  Error:Unexpected reading')
+
+
+
 	def run(self):
-		channel_counts=None
 		while self.end_condition.is_set():
-			next=self.port.read(1)
+			aux= self.port.inWaiting()
+			if aux==0:
+				aux=1
+			logging.info(self.name+': inWaiting()= '+`aux`)
+			next=self.port.read(aux)
 			if not next:
 				break
-	
-			#------------------Overflows
-			if ((ord(next[0]) & 0b11000000)== 0b00000000):
-				self.status='Ovbyte1'
-				if channel_counts!=None:
-					channel_counts=None
-					logging.info(self.name+': Error:Error while reading the counts.')
-				overflow=[0 for x in xrange(18)]
-				overflow[0:6]=[int(x) for x in '{0:06b}'.format(ord(next[0]) & 0b00111111)[::-1]]
-				continue
-
-			if self.status=='Ovbyte1' and ((ord(next[0]) & 0b10000000) == 0b10000000):
-				self.status='Ovbyte2'
-				overflow[6:13]=[int(x) for x in '{0:07b}'.format(ord(next[0]) & 0b01111111)[::-1]]
-				continue
-
-			if self.status=='Ovbyte2' and ((ord(next[0]) & 0b10000000) == 0b10000000):
-				self.status='bytex'
-				overflow[13:]=[int(x) for x in '{0:05b}'.format(ord(next[0]) & 0b00011111)[::-1]]
-				overflow_general=(ord(next[0]) & 0b00100000) >> 5
-				overflow_almost_full=(ord(next[0]) & 0b01000000) >> 6
-	
-				print 'Overflow: ',overflow
-				self.shared_events_data[3][:]=map(operator.add, self.shared_events_data[3],overflow)
-				#  TODO Decide what to do with the overflow_general and almost_full data...
-				continue
-		
-			#----------------Pulse Widths
-    			if ((ord(next[0]) & 0b11100000) == 0b01000000):
-        			self.status="byte1"
-				if channel_counts!=None:
-					channel_counts=None
-					logging.info(self.name+': Error:Error while reading the counts.')
-        			channel=ord(next[0]) & 0b00011111
-        			continue
-
-    			if  self.status=="byte1" and ((ord(next[0]) & 0b10000000) == 0b10000000):
-        			self.status="byte2"
-        			count06=ord(next[0]) & 0b01111111
-        			continue
-
-    			if self.status=="byte2" and ((ord(next[0]) & 0b10000000) == 0b10000000):
-        			self.status="bytex"
-        			count711=ord(next[0]) & 0b00011111
-        			pulse_type=(ord(next[0]) & 0b00100000) >> 5
-        			pulse_width_ticks=count06 + (count711 << 7)
-
-				if pulse_type==0:
-					#self.low_events[channel] +=1
-					self.shared_events_data[2][channel] +=1
-				if pulse_type==1:
-					#self.histo_data[channel][pulse_width_ticks>>5] +=1
-					self.shared_events_data[1][channel][pulse_width_ticks>>5] +=1
-					#self.events_min[channel] +=1
-					self.shared_events_data[0][channel] +=1
-				
-
-        			print datetime.datetime.now().time(),"Ch:",channel,"Pulse type:",pulse_type," Pulse width:",pulse_width_ticks,\
-                			float(pulse_width_ticks) / 50.0,"useg"
-        			continue
-
-
-			#----------------Counts
-			if ((ord(next[0]) & 0b11100000) == 0b01100000):
-				self.status='Contbyte3'
-				print 'Counts transmision started...'
-				if channel_counts!=None:
-					logging.info(self.name+': Error:Error while reading the counts.')
-				channel_counts=0
-				counts_val=0
-				counts_values=[0 for x in xrange(18)]
-				continue
-
-			if self.status=='Contbyte3' and ((ord(next[0]) & 0b10000000) == 0b10000000):
-				self.status='Contbyte1'
-				counts_val=ord(next[0]) & 0b01111111
-				continue
-
-			if self.status=='Contbyte1' and ((ord(next[0]) & 0b10000000) == 0b10000000):
-				self.status='Contbyte2'
-				counts_val=counts_val+((ord(next[0]) & 0b01111111) << 7)
-				continue
-
-			if self.status=='Contbyte2' and ((ord(next[0]) & 0b10000000) == 0b10000000):
-				self.status='Contbyte3'
-				counts_val=counts_val+((ord(next[0]) & 0b00000011) << 14)
-				counts_values[channel_counts]=counts_val
-				if channel_counts==17:
-					self.shared_counts_data[:]=counts_values[:]
-					self.counts_condition.release()
-
-					channel_counts=None
-					self.status='bytex'
-				else:
-					channel_counts +=1
-				continue
-
-
-
-			#--------Error, we have received something that wasnt expected
-			if channel_counts!=None:
-				logging.info(self.name+': Error:Error while reading the counts.')
-			self.status='bytex'
-			#  TODO maybe restart all the varibles {channel_counts}
-			#  TODO keep acounts of unexpected reads
-			logging.info(self.name+':  Error:Unexpected reading')
-
-
-
-
+			for data in next:
+				self.process(data)
