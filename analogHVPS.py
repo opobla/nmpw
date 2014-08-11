c929719e54ef68bfd52568ff6c2f2748c7d8e743
hristoivanov <hristo.idgh@gmail.com>
2014-08-11 11:20:50 +0000

    BBIO.ADC seems to have a bug

--- analogHVPS.py
+++ analogHVPS.py
@@ -4,12 +4,23 @@ def analogHVPS_init():
 	ADC.setup()
 
 def analogHVPS_read():
-	#Read returns values between 0.0 and 1.0. In volatage those two values correspond to 0.0V and 1.8V. Multiplying by 1800 the read value we convert it to millivolts
-	hvps1=ADC.read("P9_39")*1800
-	hvps2=ADC.read("P9_40")*1800
-	hvps3=ADC.read("P9_37")*1800
-	hvps4=ADC.read("P9_36")*1800
+	# Read returns values between 0.0 and 1.0. In volatage those two values correspond to 0.0V and 1.8V. Multiplying by 1800 the read value we convert it to millivolts
+
+	# I hate this........
+	# There is a bug in BBIO.ADC and values must be read twice in order to obtain the correct value. 
+	#  TODO keep track if this bug.   https://learn.adafruit.com/setting-up-io-python-library-on-beaglebone-black/adc
+	hvps1=ADC.read("P9_39")*1800.0
+	hvps1=ADC.read("P9_39")*1800.0
+
+	hvps2=ADC.read("P9_40")*1800.0
+	hvps2=ADC.read("P9_40")*1800.0
+
+	hvps3=ADC.read("P9_37")*1800.0
+	hvps3=ADC.read("P9_37")*1800.0
+
+	hvps4=ADC.read("P9_38")*1800.0
+	hvps4=ADC.read("P9_38")*1800.0
 	#  TODO Maybe convert the millivolts to something more meaningfull to us and return that value
-	return hvps1, hvps2, hvp3, hvp4
+	return hvps1, hvps2, hvps3, hvps4
 
 
