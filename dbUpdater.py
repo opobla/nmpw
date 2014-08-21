4b81c5ada7f6216bf6ba2f428d28b4e3cbc1a775
hristoivanov <hristo.idgh@gmail.com>
2014-08-21 12:36:58 +0000

    With this the Remote database updater is pretty much done

--- dbUpdater.py
+++ dbUpdater.py
@@ -17,9 +17,26 @@ class dbUpdater(threading.Thread):
 						passwd= self.remote['pass'], # your password
 						db= 	self.remote['database']) # name of the data base
 
-		# TODO conn_remote last_from_remote="select start_date_time from bintable order by start_date_time desc limit 1
-		# TODO conn local  to_apdate="select * from binTable where start_date_time > "+ last_from_remote +" limit 50
-		# TODO conn remote  Update with to_update
 
-		print 'Nice'
+		cur_rem = conn_remote.cursor()
+		cur_rem.execute("SELECT max(start_date_time) FROM binTable")
+		row = cur_rem.fetchone()
+		last_data_remote=row[0]
 
+		print last_data_remote
+		if last_data_remote==None:
+			last_data_remote='2000-01-01 00:00:00'
+		else:
+			last_data_remote=str(last_data_remote)
+
+		cur_loc = conn_local.cursor()    
+		cur_loc.execute("SELECT * FROM binTable where start_date_time > '"+ last_data_remote +"' limit 60*12")
+
+		rows = []
+		while True:	      
+			row = cur_loc.fetchone()
+			if row == None:
+				break
+			rows.append((str(row[0]),row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9],row[10],row[11],row[12],row[13],row[14],row[15],row[16],row[17],row[18],row[19],row[20],row[21],row[22],row[23],row[24],row[25]))
+		
+		cur_rem.executemany("INSERT INTO binTable (start_date_time, ch01, ch02, ch03, ch04, ch05, ch06, ch07, ch08, ch09, ch10, ch11, ch12, ch13, ch14, ch15, ch16, ch17, ch18, hv1, hv2, hv3, hv4, temp_1, temp_2, atmPressure) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)", rows)
