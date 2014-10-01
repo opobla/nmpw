52446c43f0032747b6b0ceecf0a9a8e5a208061e
hristoivanov <hristo.idgh@gmail.com>
2014-10-01 13:56:11 +0000

    Argument Validation for the db_updater

--- validate_arguments.py
+++ validate_arguments.py
@@ -17,7 +17,15 @@ def validate_arguments(args):
 	if (args.hvps_type=='digital' or args.barometer_type=='bm35') and (args.serial_port_control==None or args.serial_port_sensors==None):
 		end('In order to read data from a digital hvps we need two ports, one for control and one for data')
 
-	#  TODO dbupdater arguemnts.....
+	if args.db_updater_enabled==True:
+		if args.local_db==None or args.local_db!=args.database:
+			end('Incorrect local_db')
+		if args.remote_db_host==None:
+			end('If db_updater is enabled a remote_db_host must be set')
+		if args.remote_db_user==None or args.remote_db_pass==None:
+			end('A user and pass must be given for the remote database')
+		if args.remote_db_db
+			end('The remote database name must be specified')
 
 
 def end(error_msg):
