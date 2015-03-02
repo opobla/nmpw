dbde4236ccb1b412c954898df410136b53348a39
Hristo Ivanov <hristo.idgh@gmail.com>
2015-03-02 16:34:50 +0100

    BM35Driver tests up to date.

--- tests/UnitTests.py
+++ tests/UnitTests.py
@@ -6,24 +6,25 @@ cov.start()
 ################################Coverage_START#####################################################
 import unittest
 from bm35_tests import bm35TestCase
-from CountsPettioner_tests import CountsPettionerTestCase
-from Reader_tests import ReaderTestCase
+#from CountsPettioner_tests import CountsPettionerTestCase
+#from Reader_tests import ReaderTestCase
 from SensorsManager_tests import SensorsManagerTestCase
-from dbUpdater_tests import dbUpdaterTestCase
-from validate_arguments_tests import validate_argumentsTestCase
+#from dbUpdater_tests import dbUpdaterTestCase
+#from validate_arguments_tests import validate_argumentsTestCase
 #from bbbDAQ_tests import bbbDAQTestCase
 
 def bm35_suite():
 	return unittest.TestLoader().loadTestsFromTestCase(bm35TestCase)
 
+def SensorsManager_suite():
+	return unittest.TestLoader().loadTestsFromTestCase(SensorsManagerTestCase)
+"""
 def CountsPettioner_suite():
 	return unittest.TestLoader().loadTestsFromTestCase(CountsPettionerTestCase)
 
 def Reader_suite():
 	return unittest.TestLoader().loadTestsFromTestCase(ReaderTestCase)
 
-def SensorsManager_suite():
-	return unittest.TestLoader().loadTestsFromTestCase(SensorsManagerTestCase)
 
 def dbUpdater_suite():
 	return unittest.TestLoader().loadTestsFromTestCase(dbUpdaterTestCase)
@@ -31,19 +32,16 @@ def dbUpdater_suite():
 def bbbDAQ_suite():
 	return unittest.TestLoader().loadTestsFromTestCase(bbbDAQTestCase)
 
-def validate_arguments_suite():
-	return unittest.TestLoader().loadTestsFromTestCase(validate_argumentsTestCase)
-
+"""
 if __name__ == '__main__':
 	#  TODO add arguments wih argparse which will allow us to test different units.
 	allTests=unittest.TestSuite()
 	allTests.addTests(bm35_suite())
-	allTests.addTests(CountsPettioner_suite())
-	allTests.addTests(Reader_suite())
+	#allTests.addTests(CountsPettioner_suite())
+	#allTests.addTests(Reader_suite())
 	allTests.addTests(SensorsManager_suite())
-	allTests.addTests(dbUpdater_suite())
-	allTests.addTests(validate_arguments_suite())
-#	allTests.addTests(bbbDAQ_suite())
+	#allTests.addTests(dbUpdater_suite())
+	#allTests.addTests(bbbDAQ_suite())
 	
 	unittest.TextTestRunner(verbosity=2).run(allTests)
 #################################################################################################
--- tests/bm35_tests.py
+++ tests/bm35_tests.py
@@ -2,63 +2,56 @@ import unittest
 from mock import MagicMock
 import sys
 sys.path.append('.')
-import bm35
+import BM35Driver
 
 class bm35TestCase(unittest.TestCase):
 	def test_Equality_compute_crc(self):
-		response=bm35.bm35_compute_crc('AAA')
+		response=BM35Driver.bm35_compute_crc('AAA')
 		self.assertEqual(response,'AAA195')
 		
-		response=bm35.bm35_compute_crc('HiThere')
+		response=BM35Driver.bm35_compute_crc('HiThere')
 		self.assertEqual(response,'HiThere681')
 
-		response=bm35.bm35_compute_crc('aaa')
+		response=BM35Driver.bm35_compute_crc('aaa')
 		self.assertNotEqual(response,'aaa195')
 
 	def test_Equality_parse_pressure_answer(self):
-		response=bm35.bm35_parse_pressure_answer('M99D08.02.1999,11:40,96502,96555,0785\r\n')
+		response=BM35Driver.bm35_parse_pressure_answer('M99D08.02.1999,11:40,96502,96555,0785\r\n')
 		self.assertEqual(response,{'date':'1999-02-08 11:40:00','meanPressure':96502,'instantPressure':96555})
 
-		response=bm35.bm35_parse_pressure_answer('M99D17.07.2014,12:47,85661,11555,0768\r\n')
+		response=BM35Driver.bm35_parse_pressure_answer('M99D17.07.2014,12:47,85661,11555,0768\r\n')
 		self.assertEqual(response,{'date':'2014-07-17 12:47:00','meanPressure':85661,'instantPressure':11555})
 
 	def test_Errors_parse_pressure_answer(self):
 		#Mock the logging
 		logging=MagicMock()
 		logging.info.return_value=True
-
-		# Make the CountsPettioner use the mocked logging
 		sys.modules['logging'] = logging
-		import bm35
-		reload(bm35)
+		import BM35Driver
+		reload(BM35Driver)
 
-		response=bm35.bm35_parse_pressure_answer('M99D08.02.1999,11:40,96502,96555,0785')
+		response=BM35Driver.bm35_parse_pressure_answer('M99D08.02.1999,11:40,96502,96555,0785')
 		self.assertEqual(response,{'date':-1,'meanPressure':-1,'instantPressure':-1})
 		logging.info.assert_called_with('Bm35: Invalid answer to parse. Bad format')
 
-		response=bm35.bm35_parse_pressure_answer('M99D17.07.2014,12:47,85661,11555,0111\r\n')
+		response=BM35Driver.bm35_parse_pressure_answer('M99D17.07.2014,12:47,85661,11555,0111\r\n')
 		self.assertEqual(response,{'date':-1,'meanPressure':-1,'instantPressure':-1})
 		logging.info.assert_called_with('Bm35: Invalid answer to parse. Wrong CRC')
-
-
-		#self.assertRaisesRegexp(ValueError,'Invalid answer to parse', bm35.bm35_parse_pressure_answer, 'A incorrect string, just to test')
-		#self.assertRaisesRegexp(ValueError,'Answers crc is invalid', bm35.bm35_parse_pressure_answer, 'M99D17.07.2014,12:47,85661,11555,0444')
-
 
 	def test_Writting_request_pressure_reading(self):
 		import serial
 		mock_port=serial.Serial()
 		mock_port.write=MagicMock(return_value='Hi')
-		bm35.bm35_request_pressure_reading(mock_port)
+		BM35Driver.bm35_request_pressure_reading(mock_port)
 
-		mock_port.write.assert_called_with(bm35.bm35_compute_crc('A00Q1')+'\r\n')
+		mock_port.write.assert_called_with(BM35Driver.bm35_compute_crc('A00Q1')+'\r\n')
 
 	def test_Writting_request_1min_reading_period(self):
 		import serial
 		mock_port=serial.Serial()
 		mock_port.write=MagicMock(return_value='Hi')
-		bm35.bm35_request_1min_reading_period(mock_port)
+		BM35Driver.bm35_request_1min_reading_period(mock_port)
 
-		mock_port.write.assert_called_with(bm35.bm35_compute_crc('A00I10')+'\r\n')
+		mock_port.write.assert_called_with(BM35Driver.bm35_compute_crc('A00I10')+'\r\n')
 
 
