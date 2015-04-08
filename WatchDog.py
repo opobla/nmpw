44c9c21343198fdab6d2015bbd9990ccbf1bdab6
hristoivanov <hristo.idgh@gmail.com>
2015-04-08 08:05:29 +0000

    The watchdog now checks if there is data present, if not the board is restarted.

--- WatchDog.py
+++ WatchDog.py
@@ -1,10 +1,38 @@
 import time
 import os
+import NMDA
+import sqlite3
+
+def get_last():
+	cursor = conn_local.cursor()
+	cursor.execute("SELECT max(start_date_time) FROM binTable")
+	row = cursor.fetchone()
+	last_data=row[0]
+	if last_data==None:
+		last_data='2000-01-01 00:00:00'
+	else:
+		last_data=str(last_data)
+	return last_data
+
 
 # First of all we give big priority to this process
 os.nice(20)
+args=NMDA.create_parser().parse_args()
+
+conn_local 	= sqlite3.connect(args.database)
+last_data	= get_last()
+cont		= 0
+
 wd = open("/dev/watchdog", "w+")
-while 1:
+while True:
 	wd.write("\n")
 	wd.flush()
 	time.sleep(20)
+	cont+=1
+
+	if cont >= 15:					#15*20secs=5mins
+		cont		= 0
+		curr_last	= get_last()
+		if curr_last == last_data:
+			break
+		last_data 	= curr_last
